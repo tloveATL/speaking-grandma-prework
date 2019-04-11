a9618a6010cb9841242b55c9678c248d53032210
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-11 16:09:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,6 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == 
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO P"
 end
